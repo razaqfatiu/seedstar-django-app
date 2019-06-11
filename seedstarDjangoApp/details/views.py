bd327f25bf118ba3f0a694b14f8bdabf6abc6a59
AbdRozaq Iwashokun <ade.iwashokun@triatronltd.com>
2019-06-11 14:45:41 +0100

    Almost done with add Route

--- seedstarDjangoApp/details/views.py
+++ seedstarDjangoApp/details/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Details
-
+from .models import Details as details
+from details.forms import Details
 # Create your views here.
 
 def index(request):
     return render(request, "details/welcome.html")
 def list(request):
     context = {
-        "details": Details.objects.all()
+        "details": details.objects.all()
     }
     return render(request, "details/list.html", context)
-def add(request):
-    return HttpResponse("Add")
+class add():
+    template_name = "details/add.html"
+    def get(self, request):
+        form = Details()
+        return render(request, self.template_name, {'form': form})
+    def post(self, request):
+        form = Details(request.POST)
+        if form.is_valid():
+            form.save()
+            text = form.cleaned_data['name', 'email']
+
+        return render(request, self.template_name, {'form': form})
